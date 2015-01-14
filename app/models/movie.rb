68efd95beb3b5bf224c0933c1a27cc98968bb3f9
prufrock123 <shawnmllr86@gmail.com>
2015-01-14 23:03:34 +0000

    temp fix for model validations so I can show working deploy prototype.

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,7 +1,7 @@
 class Movie < ActiveRecord::Base
   validates :title, presence: true, length: { in: 1..50 }
-  validates :format, presence: true, 
-  validates :length, presence: true, length { in: }
+  validates :format, presence: true
+  validates :length, presence: true
   validates :release, presence: true, numericality: { only_integer: true }
   validates :rating, presence: true, numericality: { only_integer: true }
   
