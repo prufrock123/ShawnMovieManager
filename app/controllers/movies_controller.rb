381611bdc52f612de405badfaab822c6f7c43cc4
prufrock123 <shawnmllr86@gmail.com>
2015-01-15 00:43:05 +0000

    Adds update and destroy methods.

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -7,25 +7,42 @@ class MoviesController < ApplicationController
   def new
     @movie = Movie.new
   end
+
+  def edit
+    @movie = Movie.find(params[:id])
+  end  
   
   def create
     @movie = Movie.new(movie_params)
     
-    @movie.save
-    redirect_to @movie
+    if @movie.save
+      redirect_to @movie
+    else
+      render 'new'
+    end
+  end
+  
+  def update
+    @movie = Movie.find(params[:id])
+    
+    if @movie.update(movie_params)
+      redirect_to @movie
+    else
+      render 'edit'
+    end
   end
 
   def show
     @movie = Movie.find(params[:id])
   end
-  
-  def edit
-  end
 
-  def update
-  end
 
-  def delete
+
+  def destroy
+    @movie = Movie.find(params[:id])
+    @movie.destroy
+    
+    redirect_to movies_path
   end
   
   private
