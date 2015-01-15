935e56942cf07317da0dbc9fb5ba6393a3a7a5c1
prufrock123 <shawnmllr86@gmail.com>
2015-01-15 06:49:23 +0000

    Adds validations for movie length.

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,9 +1,18 @@
+class MyLengthValidator < ActiveModel::Validator
+  def validate(record)
+    unless record.length.strftime("%H") <= "08" 
+      record.errors[:base] << "Testing error"
+    end
+  end
+end
+
 class Movie < ActiveRecord::Base
+#   include ActiveModel::Validations
+  validates_with MyLengthValidator
   validates :title, presence: true, length: { in: 1..50 }
   validates :format, presence: true
   validates :length, presence: true
   validates :release, presence: true, numericality: { only_integer: true }
-#   validates :rating, presence: true, numericality: { only_integer: true }
   include RankedModel
   
   ranks :row_order
